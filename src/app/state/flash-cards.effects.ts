396db0c30e4ef41ef8badb203b21d2d284ec04fe
Stephen <teamgreensf@gmail.com>
2024-12-10 14:54:25 -0800

    API call example with comments

--- src/app/state/flash-cards.effects.ts
+++ src/app/state/flash-cards.effects.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { catchError, from, map, of, switchMap } from 'rxjs';
+import { addOneFlashCardAPI, addOneFlashCardAPIFailure, addOneFlashCardAPISuccess } from './flash-cards.reducer';
 
 @Injectable()
 export class FlashCardsEffects {
@@ -12,20 +13,21 @@ export class FlashCardsEffects {
     private http: HttpClient
   ) {}
 
-  // OPTIONAL TODO: Implement an effect to save flash cards to a backend
+  // TODO: Implement an effect to save flash cards to a backend
   // NOTE: This is an example of how you might implement an effect to save flash cards to a backend.
-  // You will need three new actions: addOneFlashCardAPI, addOneFlashCardAPISuccess, and addOneFlashCardAPIFailure
+  // You will need three actions: addOneFlashCardAPI, addOneFlashCardAPISuccess, and addOneFlashCardAPIFailure
   // each with their own appropriate parameters.
+  // This effect will trigger when `addOneFlashCardAPI` is dispatched.
+  addOneFlashCardAPI$ = createEffect(() => this.actions$.pipe(
+    ofType(addOneFlashCardAPI),
+    switchMap(({ flashCard }) => 
+      from(this.http.post('/some-api/flash-cards/add', flashCard)).pipe(
+        map(() => addOneFlashCardAPISuccess({ flashCard })),
+        catchError(error => of(addOneFlashCardAPIFailure({ flashCard, error })))
+      )
+    )
+  ));
+
   // Similarly, you should fetch the flash cards from the backend when the application starts via
   // an effect which triggers on application load.
-  //
-  // addOneFlashCardAPI$ = createEffect(() => this.actions$.pipe(
-  //   ofType(addOneFlashCardAPI),
-  //   switchMap(({ flashCard }) => 
-  //     from(this.http.post('/api/flash-cards', flashCard)).pipe(
-  //       map(() => addOneFlashCardAPISuccess({ flashCard })),
-  //       catchError(error => of(addOneFlashCardAPIFailure({ flashCard, error })))
-  //     )
-  //   )
-  // )
 }
--- src/app/state/flash-cards.reducer.ts
+++ src/app/state/flash-cards.reducer.ts
@@ -16,13 +16,43 @@ export const addOneFlashCard = createAction(
   props<{ flashCard: FlashCard }>(),
 );
 
-// TODO: Add a delete and update action
+/**
+ * @description Add one flashcard to the database, then
+ * update the state with the new flash card
+ */
+export const addOneFlashCardAPI = createAction(
+  '[Flash Cards] Add One (API)',
+  props<{ flashCard: FlashCard }>(),
+);
+/**
+ * @description This action is called when the API call
+ * to add a flash card is successful
+ */
+export const addOneFlashCardAPISuccess = createAction(
+  '[Flash Cards] Add One (API) Success',
+  props<{ flashCard: FlashCard }>(),
+);
+/**
+ * @description This action is called when the API call
+ * fails to add a flash card
+ */
+export const addOneFlashCardAPIFailure = createAction(
+  '[Flash Cards] Add One (API) Failure',
+  props<{ flashCard: FlashCard, error: any }>(),
+);
+
+// TODO: Add a delete and update action (with API versions)
 
 export const reducer = createReducer(
   initialState,
 
   // Add one flash card
   on(addOneFlashCard, (state, { flashCard }) =>
+    adapter.addOne(flashCard, state),
+  ),
+
+  // Add a flash card to our store after successful creation at the backend
+  on(addOneFlashCardAPISuccess, (state, { flashCard }) =>
     adapter.addOne(flashCard, state),
   ),
 );
